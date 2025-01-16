3747b006f439c0fac87a631e8fab28745aec7481
Kay-Rodriguez <151900296+Kay-Rodriguez@users.noreply.github.com>
2025-01-16 07:15:54 -0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Variables de conexión a la base de datos
+        
         String url = "jdbc:mysql://localhost:3306/calificaciones";
         String user = "root";
         String password = "123456";
@@ -21,7 +21,7 @@ public class Main {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conectado a la base de datos");
 
-            // Crear la ventana principal para el login
+     
             JFrame frame = new JFrame("Login");
             frame.setContentPane(new login().mainPanel);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,7 +34,7 @@ public class Main {
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Error al conectar con la base de datos");
         } finally {
-            // Asegurarse de cerrar la conexión
+        
             try {
                 if (conn != null) {
                     conn.close();
