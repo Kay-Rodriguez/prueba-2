45b8587bdac7951ec7732733cac9bced408221cf
Kay-Rodriguez <151900296+Kay-Rodriguez@users.noreply.github.com>
2025-01-16 07:21:08 -0500

    Add files via upload

---
+++ calificaciones.sql
@@ -0,0 +1,30 @@
+ create database calificaciones;
+ use calificaciones;
+ CREATE TABLE usuarios (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    username VARCHAR(50) NOT NULL UNIQUE,
+
+    password VARCHAR(255) NOT NULL
+
+);
+ CREATE TABLE estudiantes (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    cedula VARCHAR(10) NOT NULL UNIQUE,
+
+    nombre VARCHAR(100) NOT NULL,
+
+    estudiante1 DOUBLE NOT NULL,
+
+    estudiante2 DOUBLE NOT NULL,
+
+    estudiante3 DOUBLE NOT NULL,
+
+    estudiante4 DOUBLE NOT NULL,
+
+    estudiante5 DOUBLE NOT NULL
+
+);
